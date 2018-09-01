15158308add9cb5397a41ebca407f3021903046e
mainulhossain <mainulhossain@gmail.com>
2018-09-02 01:48:17 +0600

    revert to previous versions

--- app/jobs.py
+++ app/jobs.py
@@ -1,19 +1,19 @@
+from celery.contrib.abortable import AbortableTask, AbortableAsyncResult
+from celery.states import state, PENDING, SUCCESS
+
+from flask_login import current_user
+
+from datetime import datetime
 import os
 import random
 import time
-
-from celery.contrib.abortable import AbortableTask, AbortableAsyncResult
-from celery.states import state, PENDING, SUCCESS
-#from flask_login import current_user
-from flask import has_request_context, request, g
+import json
 
 from config import Config
-
 from . import celery, create_app
 from .biowl.dsl.parser import PhenoWLParser, PythonGrammar
 from .biowl.timer import Timer
 from .models import Runnable
-from manage import app
 
 class ContextTask(AbortableTask):
     abstract = True
@@ -25,105 +25,22 @@ class ContextTask(AbortableTask):
         with app.app_context():
             return AbortableTask.__call__(self, *args, **kwargs)
 
-# class RequestContextTask(AbortableTask):
-#     """Celery task running within Flask test request context.
-#     Expects the associated Flask application to be set on the bound
-#     Celery application.
-#     """
-#     abstract = True
-#     def __call__(self, *args, **kwargs):
-#         """Execute task."""
-#         with self.app.flask_app.test_request_context():
-#             self.app.flask_app.try_trigger_before_first_request_functions()
-#             self.app.flask_app.preprocess_request()
-#             res = AbortableTask.__call__(self, *args, **kwargs)
-#             self.app.flask_app.process_response(
-#                 self.app.flask_app.response_class())
-#             return res
-
-__all__ = ['RequestContextTask']
-
-
 class RequestContextTask(AbortableTask):
-    """Base class for tasks that originate from Flask request handlers
-    and carry over most of the request context data.
-    This has an advantage of being able to access all the usual information
-    that the HTTP request has and use them within the task. Pontential
-    use cases include e.g. formatting URLs for external use in emails sent
-    by tasks.
+    """Celery task running within Flask test request context.
+    Expects the associated Flask application to be set on the bound
+    Celery application.
     """
     abstract = True
-
-    #: Name of the additional parameter passed to tasks
-    #: that contains information about the original Flask request context.
-    CONTEXT_ARG_NAME = '_flask_request_context'
-    GLOBALS_ARG_NAME = '_flask_global_proxy'
-    GLOBAL_KEYS = ['user']
-
     def __call__(self, *args, **kwargs):
-        """Execute task code with given arguments."""
-        call = lambda: super(RequestContextTask, self).__call__(*args, **kwargs)
-
-        # set context
-        context = kwargs.pop(self.CONTEXT_ARG_NAME, None)
-        gl = kwargs.pop(self.GLOBALS_ARG_NAME, {})
-
-        if context is None or has_request_context():
-            return call()
-
-        with app.test_request_context(**context):
-            # set globals
-            for i in gl:
-                setattr(g, i, gl[i])
-            # call
-            result = call()
-            # process a fake "Response" so that
-            # ``@after_request`` hooks are executed
-            # app.process_response(make_response(result or ''))
-
-        return result
-
-    def apply_async(self, args=None, kwargs=None, **rest):
-        self._include_request_context(kwargs)
-        self._include_global(kwargs)
-        return super(RequestContextTask, self).apply_async(args, kwargs, **rest)
-
-    def apply(self, args=None, kwargs=None, **rest):
-        self._include_request_context(kwargs)
-        self._include_global(kwargs)
-        return super(RequestContextTask, self).apply(args, kwargs, **rest)
-
-    def retry(self, args=None, kwargs=None, **rest):
-        self._include_request_context(kwargs)
-        self._include_global(kwargs)
-        return super(RequestContextTask, self).retry(args, kwargs, **rest)
-
-    def _include_request_context(self, kwargs):
-        """Includes all the information about current Flask request context
-        as an additional argument to the task.
-        """
-        if not has_request_context():
-            return
-
-        # keys correspond to arguments of :meth:`Flask.test_request_context`
-        context = {
-            'path': request.path,
-            'base_url': request.url_root,
-            'method': request.method,
-            'headers': dict(request.headers),
-        }
-        if '?' in request.url:
-            context['query_string'] = request.url[(request.url.find('?') + 1):]
-
-        kwargs[self.CONTEXT_ARG_NAME] = context
-
-    def _include_global(self, kwargs):
-        d = {}
-        for z in self.GLOBAL_KEYS:
-            if hasattr(g, z):
-                d[z] = getattr(g, z)
-        kwargs[self.GLOBALS_ARG_NAME] = d
-                
+        """Execute task."""
+        with self.app.flask_app.test_request_context():
+            self.app.flask_app.try_trigger_before_first_request_functions()
+            self.app.flask_app.preprocess_request()
+            res = AbortableTask.__call__(self, *args, **kwargs)
+            self.app.flask_app.process_response(
+                self.app.flask_app.response_class())
+            return res
+        
 def long_task(self):
     """Background task that runs a long function with progress reports."""
     verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
@@ -142,7 +59,7 @@ def long_task(self):
         time.sleep(1)
     return {'current': 100, 'total': 100, 'status': 'Task completed!', 'result': 42}
 
-@celery.task(bind=True, base=RequestContextTask)#, base = AbortableTask
+@celery.task(bind=True, base=ContextTask)#, base = AbortableTask
 def run_script(self, machine, script, args):
     parserdir = Config.BIOWL
     curdir = os.getcwd()

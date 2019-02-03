eb865cafc0d2f9fd47237f4265f441fd29e66ec3
mainulhossain <mainulhossain@gmail.com>
2019-02-03 16:54:21 +0600

    conda service for server

--- app/biowl/libraries/qiime/conda_service.sh
+++ app/biowl/libraries/qiime/conda_service.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-source "/home/mainul/miniconda3/bin/activate" "/home/mainul/miniconda3/envs/qiime2-2018.11"
-#calling the required tools here... such as 
-"/home/mainul/miniconda3/envs/qiime2-2018.11/bin/qiime" $1
-"/home/mainul/miniconda3/bin/deactivate"
-
+source "/home/phenodoop/miniconda3/bin/activate" "/home/phenodoop/miniconda3/envs/qiime2-2018.11"
+#calling the required tools here... such as
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+"/home/phenodoop/miniconda3/envs/qiime2-2018.11/bin/qiime" $1
+"/home/phenodoop/miniconda3/bin/deactivate"

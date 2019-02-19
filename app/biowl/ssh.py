feab12809abeae612966c0ec5ba0df4e6ffb7f71
mainulhossain <mainulhossain@gmail.com>
2019-02-19 23:02:50 +0600

    ssh_hadoop_command path not working

--- app/biowl/ssh.py
+++ app/biowl/ssh.py
@@ -2,17 +2,17 @@ import paramiko
 from scp import SCPClient
 
 def get_ssh_client(myhost, myuser, mypassword = None, keyfile=None):
-    k = None
-    if keyfile:
-        k = paramiko.RSAKey.from_private_key_file(keyfile)
-        if not k:
-            raise ValueError("RSAKey from private key file did not work.")
+#     k = None
+#     if keyfile:
+#         k = paramiko.RSAKey.from_private_key_file(keyfile)
+#         if not k:
+#             raise ValueError("RSAKey from private key file did not work.")
     
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
-    if k:
-        ssh_client.connect(hostname=myhost, username=myuser, pkey = k)
+    if keyfile:
+        ssh_client.connect(hostname=myhost, username=myuser, key_filename=keyfile)#pkey = k)
     else:
         ssh_client.connect(hostname=myhost, username=myuser, password=mypassword)
     return ssh_client
@@ -32,7 +32,9 @@ def ssh_hadoop_command(myhost, myuser, keyfile, command):
 #     """
 
     #_, stdout, _ = ssh_client.exec_command('export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop;' + command)
-    _, stdout, _ = ssh_client.exec_command('export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop;' + command)
+    #_, stdout, _ = ssh_client.exec_command('export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop;' + command)
+    _, stdout, _ = ssh_client.exec_command('export HADOOP_HOME=/usr/local/hadoop; export HADOOP_SSH_OPTS="-i /home/hadoop/.ssh/hadoop_rsa";export SPARK_HOME=/usr/local/spark;export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop;export LD_LIBRARY_PATH=/usr/local/hadoop/lib/native;export JAVA_HOME=/usr/lib/jvm/java-8-oracle;export JAVA_LIBRARY_PATH=$HADOOP_HOME/lib/native:$JAVA_LIBRARY_PATH;export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native;export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native";export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$JAVA_HOME/bin:$SPARK_HOME/bin:$LD_LIBRARY_PATH;' + command)
+
     exit_status = stdout.channel.recv_exit_status() # Channel.exit_status_ready for non-blocking call
     ssh_client.close()
     return exit_status

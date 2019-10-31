5f57358dc4fa3ec718ec42c85db8d58b5207a9a9
mainulhossain <mainulhossain@gmail.com>
2019-10-31 15:18:29 +0600

    Update Metadata: add key value

--- app/static/app.js/metadataViewModel.js
+++ app/static/app.js/metadataViewModel.js
@@ -9,27 +9,32 @@ function MetaDataViewModel() {
     self.annotations = new ko.observableDictionary(); // list of strings
     self.properties = new ko.observableDictionary(); // keyvalue
     self.mimetypes = new ko.observableDictionary();
-    
+
     self.save = function() {
         
         var formdata = new FormData();
-        
+
         formdata.append('save', self.path());
         formdata.append('filename', self.filename());
         formdata.append('visualizers', ko.toJSON(self.visualizers));
         formdata.append('annotations', ko.toJSON(self.annotations));
         formdata.append('mimetypes', ko.toJSON(self.mimetypes));
         formdata.append('properties', ko.toJSON(self.properties));
-        
+
         ajaxcalls.form(self.metadataURI, 'POST', formdata).done(function(data) {
-            debugger;
-        }).fail(function (jqXHR, a,b) {  
-            debugger;
+
+        }).fail(function (jqXHR, a,b) {
+                       
         });
     } 
     
     self.load = function(path) {
         
+        self.newPropCounter = 0;
+        self.newVisCounter = 0;
+        self.newAnntnCounter = 0;
+        self.newMimeCounter = 0;
+
         self.path(path);
         self.filename(path.replace(/^.*[\\\/]/, ''));
         ajaxcalls.simple(self.metadataURI, 'GET', {'load': path}).done(function(data) {
@@ -71,7 +76,49 @@ function MetaDataViewModel() {
         }).always(function(x, textStatus, jqXHR){
             self.historyLoading = false;
         });
-    }
+    };
+
+    self.newPropCounter = 0;
+    self.newVisCounter = 0;
+    self.newAnntnCounter = 0;
+    self.newMimeCounter= 0;
+
+    self.addNewProperty = function () {  
+        ++self.newPropCounter;
+        self.properties.push('new key '+ self.newPropCounter , 'new value '+ self.newPropCounter);
+    };
+
+    self.removeProperty = function (data, ev) { 
+        self.properties.remove(data.key());
+    };
+
+    self.addNewVisualizer = function () {  
+        ++self.newVisCounter;
+        self.visualizers.push('new key '+ self.newVisCounter , 'new value '+ self.newVisCounter);
+    };
+
+    self.removeVisualizer = function (data, ev) {  
+        self.visualizers.remove(data.key());
+    };
+
+    self.addNewAnnotation = function () {  
+        ++self.newAnntnCounter;
+        self.annotations.push('new key '+ self.newAnntnCounter , 'new value '+ self.newAnntnCounter);
+    };
+    
+    self.removeAnnotation = function (data, ev) {  
+        self.annotations.remove(data.key());
+    };
+
+    self.addNewMimeType = function () {
+        ++self.newMimeCounter;  
+        self.mimetypes.push('new key '+ self.newMimeCounter , 'new value '+ self.newMimeCounter);
+    };
+
+    self.removeMimeType = function (data, ev) {  
+        self.mimetypes.remove(data.key());
+    };
+
 }
 
 var metadataViewModel = new MetaDataViewModel();

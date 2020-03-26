6134f4426f0abe194b7b012c264600af49329305
mainulhossain <mainulhossain@gmail.com>
2020-03-26 18:36:20 +0600

    webhook for vizsciflow

--- pullwebhook.sh
+++ pullwebhook.sh
@@ -5,4 +5,8 @@ GIT_DIR=$2
 BRANCH=$3
 
 git --work-tree=$TARGET --git-dir=$GIT_DIR checkout -f $BRANCH
+#git --work-tree=$TARGET --git-dir=$GIT_DIR pull
 
+# restart the web service; you may need to restart nginx.
+# for vizsciflow, supervisor does it.
+#echo sr-hadoop | sudo -S supervisorctrl restart all

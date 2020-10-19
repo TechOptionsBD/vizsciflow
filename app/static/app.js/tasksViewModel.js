e7030499e3731f4d0bd0600728c944dda3c38263
Sultan-Bonny <mohaimin.bonny@gmail.com>
2020-10-19 17:00:50 +0600

    implemented heatmap with static data

--- app/static/app.js/tasksViewModel.js
+++ app/static/app.js/tasksViewModel.js
@@ -512,33 +512,27 @@ function TasksViewModel() {
     }
     
     function createHeatMapChart() {
-        // let ctx = document.getElementById('canvasHeatMap').getContext('2d');
-        // let data = {
-        //     labels : ['0h','1h','2h','3h','4h','5h','6h','7h','8h','9h','10h','11h'],
-        //     datasets : [
-        //       {
-        //         label: 'Monday',
-        //         data: [8, 6, 5, 7, 9, 8, 1, 6, 3, 3, 8, 7]
-        //       },
-        //       {
-        //         label: 'Tuesday',
-        //         data: [6, 8, 5, 6, 5, 5, 7, 0, 0, 3, 0, 7]
-        //       },
-        //       {
-        //         label: 'Wednesday',
-        //         data: [8, 5, 6, 4, 2, 2, 3, 0, 2, 0, 10, 8]
-        //       },
-        //       {
-        //         label: 'Thursday',
-        //         data: [4, 0, 7, 4, 6, 3, 2, 4, 2, 10, 8, 2]
-        //       },
-        //       {
-        //         label: 'Friday',
-        //         data: [1, 0, 0, 7, 0, 4, 1, 3, 4, 5, 1, 10]
-        //       }
-        //     ]
-        //   };
-        // var heatMap = new Chart(ctx).HeatMap(data, {});
+        // create data
+        var data = [
+            { x: "2010", y: "A", heat: 15 },
+            { x: "2011", y: "A", heat: 17 },
+            { x: "2012", y: "A", heat: 21 },
+            { x: "2010", y: "B", heat: 34 },
+            { x: "2011", y: "B", heat: 33 },
+            { x: "2012", y: "B", heat: 32 },
+            { x: "2010", y: "C", heat: 51 },
+            { x: "2011", y: "C", heat: 50 },
+            { x: "2012", y: "C", heat: 47 }
+        ];
+
+        // create a chart and set the data
+        chart = anychart.heatMap(data);
+
+        // set the container id
+        chart.container("proveHeatMap");
+
+        // initiate drawing the chart
+        chart.draw();
     }
 
     self.createCompView = function (view) {

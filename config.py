9b381ac3dafd16a15ef991d972270615b8e7ea91
mainulhossain <mainulhossain@gmail.com>
2021-06-02 17:18:30 -0600

    Add biowl db connection string with  username and password.

--- config.py
+++ config.py
@@ -60,7 +60,7 @@ class DevelopmentConfig(Config):
 #     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
 #         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        "postgresql+psycopg2:///biowl"
+        "postgresql+psycopg2://phenodoop:sr-hadoop@localhost/biowl"
 
 
 class TestingConfig(Config):

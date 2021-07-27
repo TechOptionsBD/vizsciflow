ed8d91dac92180e6e2a71c4c14368f9dbde52292
mainulhossain <mainulhossain@gmail.com>
2021-07-27 16:35:04 +0600

    Update config.py for database url

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ class DevelopmentConfig(Config):
 #     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
 #         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        "postgresql+psycopg2://phenodoop:sr-hadoop@localhost/biowl"
+        "postgresql://phenodoop:sr-hadoop@db:5432/biowl"
 
 
 class TestingConfig(Config):

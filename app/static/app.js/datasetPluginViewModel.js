f3b3205ba965f1ef4e3bea56492ca3c78d3241f5
bytecode <monir150227@gmail.com>
2021-11-28 17:09:20 +0600

    resolve plugin data issue in editor

--- app/static/app.js/datasetPluginViewModel.js
+++ app/static/app.js/datasetPluginViewModel.js
@@ -8,95 +8,111 @@ function DatasetPluginViewModel(scidatapath) {
     // initialize plugin
     datasetList = $("#datasetPlugin").datasetlist({
       data: self.datasetList(),
-      searchBar: true, 
+      searchBar: true,
       searchBarPlaceholder: "Search dataset in list",
-      headerText: 'SciDataManager Plugin Header Text',
+      headerText: "SciDataManager Plugin Header Text",
       onItemClick: onItemClick,
-      selectedDataItem: getSelectedDataItem, 
+      selectedDataItem: getSelectedDataItem,
       getSelectedDataItem: getSelectedDataItem,
       onDataItemDoubleClick: onDataItemDoubleClick,
       onItemDrag: onDataItemDrag,
       onItemExpand: onItemExpand,
       onItemExpanded: onItemExpanded,
-      headerClass: 'custom-dataset-header',
-      itemHeaderClass: 'custom-dataset-item-header',
-      itemBodyClass: 'custom-dataset-item-body',
-      dataItemClass: 'custom-data-item',
+      headerClass: "custom-dataset-header",
+      itemHeaderClass: "custom-dataset-item-header",
+      itemBodyClass: "custom-dataset-item-body",
+      dataItemClass: "custom-data-item",
 
       apiUrl: `${self.scidatamgrURI}/api/plugin/dataset/data?`, // set backend api for dataset data retrive
       secretKey: "6D59713374367639", //app secret key
-      userName: username //current user
+      userName: username, //current user
     });
-    
+
     // render list on UI
     datasetList.renderList();
-    datasetList.getSelectedDataItem()
-  }
+    datasetList.getSelectedDataItem();
+  };
 
-  self.renderDataset = function() {
+  self.renderDataset = function () {
     self.datasetList([]);
 
     // fetching api data
-    ajaxcalls.simple(self.scidatamgrURI + '/api/plugin/datasets', 'GET', { secret_key: '6D59713374367639', username: username, pageNum: '1', numOfItems: '50' }).done(function(dataset) {    
-      if (dataset === undefined)
-        return;
-      
-      self.datasetList(dataset.data);
-      initPlugin()
-    }).fail(function(jqXHR) {
-      showXHRText(jqXHR);
-      initPlugin()
-    });
-  }
+    ajaxcalls
+      .simple(self.scidatamgrURI + "/api/plugin/datasets", "GET", {
+        secret_key: "6D59713374367639",
+        username: username,
+        pageNum: "1",
+        numOfItems: "50",
+      })
+      .done(function (dataset) {
+        if (dataset === undefined) return;
+
+        self.datasetList(dataset.data);
+        initPlugin();
+      })
+      .fail(function (jqXHR) {
+        showXHRText(jqXHR);
+        initPlugin();
+      });
+  };
 
   self.reload = function () {
-    $("#datasetPlugin").empty()
-    self.renderDataset()
-  }
+    $("#datasetPlugin").empty();
+    self.renderDataset();
+  };
 
   self.copyToEditorDoubleClick = function (url) {
-      exmpl = `data = \'${url}\'`
-      var pos = editor.selection.getCursor();
-      editor.session.insert(pos, exmpl + "\r\n");
-      editor.focus();
-  }
+    // exmpl = `datassss = \'${url}\'`
+    // var pos = editor.selection.getCursor();
+    // editor.session.insert(pos, exmpl + "\r\n");
+    // editor.focus();
+
+    var pos = editor.selection.getCursor();
+    text = editor.session.getLine(pos.row);
+    if (text !== undefined && text.length != 0) {
+      console.log(pos, "pos");
+      editor.session.insert(pos, "'" + url + "'");
+    } else editor.session.insert(pos, "data = '" + url + "'\n");
+    editor.focus();
+    editor.getSession().reset();
+  };
 
   self.copyToEditorDrag = function (url) {
-    exmpl = `\'${url}\'`
+    exmpl = `\'${url}\'`;
     var pos = editor.selection.getCursor();
     editor.session.insert(pos, exmpl);
     editor.focus();
-}
+  };
 
   const onItemClick = function (e, data) {
-    console.log('onItemClick');
-  }
+    console.log("onItemClick");
+  };
 
   const getSelectedDataItem = function (e, data) {
-    console.log('getSelectedDataItem');
-  }
-    
+    console.log("getSelectedDataItem");
+  };
+
   const onDataItemDoubleClick = function (e, dataset) {
-    let path = dataset.path
-    let pid = dataset.dataset.pid
+    let path = dataset.path;
+    let pid = dataset.dataset.pid;
     path = path.replace("Root node", "Data");
-    path = `${self.scidatamgrURI}/api/${pid}${path}`
-    self.copyToEditorDoubleClick(path)
-  }
+    path = `${self.scidatamgrURI}/api/${pid}${path}`;
+    self.copyToEditorDoubleClick(path);
+  };
 
   const onDataItemDrag = function (e, dataset) {
-    let path = dataset.path
-    let pid = dataset.dataset.pid
+    let path = dataset.path;
+    let pid = dataset.dataset.pid;
     path = path.replace("Root node", "Data");
-    path = `${self.scidatamgrURI}/api/${pid}${path}`
-    self.copyToEditorDrag(path)
-  }
+    path = `${self.scidatamgrURI}/api/${pid}${path}`;
+    self.copyToEditorDrag(path);
+  };
 
   const onItemExpand = function (e, dataset) {
-    datasetList.lazyLoadDatasetItems(dataset)
-  }
+    datasetList.lazyLoadDatasetItems(dataset);
+  };
 
   const onItemExpanded = function (e, data) {
-    console.log('onItemExpanded');
-  }
+    console.log("onItemExpanded");
+  };
 }

9b4b849d522ddec73e78567f35313c19e37f3e45
Mainul Hossain <mainulhossain@gmail.com>
2022-03-19 14:33:56 -0600

    #19: samttols path fixed and rmdup added

--- plugins/modules/samtools/adapter.py
+++ plugins/modules/samtools/adapter.py
@@ -2,9 +2,9 @@ import os
 from os import path
 
 from app.system.exechelper import func_exec_run
-from app.dsl.argshelper import get_posix_data_args, get_posix_output_args, get_optional_posix_data_args
+from app.dsl.argshelper import get_posix_data_args, get_posix_output_args, get_optional_posix_data_args, get_posix_output_folder_args
 
-pear = path.join(path.abspath(path.dirname(__file__)), path.join('bin', 'samtools'))
+samtools = path.join(path.abspath(path.dirname(__file__)), path.join('bin', 'samtools'))
 
 def prepare_args(fs, datas, output):
     
@@ -33,10 +33,29 @@ def run_samtools_merge(context, *args, **kwargs):
     
     cmdargs = prepare_args(fs, datas, output)
     
-    _,err = func_exec_run(pear, *cmdargs)
+    _,err = func_exec_run(samtools, *cmdargs)
     
     stripped_path = fs.strip_root(output)
     if not fs.exists(output):
         raise ValueError("Merge operation failed due to error: " + err)
     
     return stripped_path
+
+def run_rmdup(context, *args, **kwargs):
+    paramindex, data, fs = get_posix_data_args(0, 'data', context, *args, **kwargs)
+    if not fs.exists(data):
+        raise ValueError("Input file {0} doesn't exist.".format(fs.strip_root(str(data))))
+
+    outdir = get_posix_output_folder_args(paramindex, 'output', fs, context, *args, **kwargs)
+
+    outpath = fs.join(outdir, 'output.bam')
+    err = ''
+    cmdargs = ['rmdup', data, outpath]
+    if fs.isfile(data):
+        _,err = func_exec_run(samtools, *cmdargs)
+
+    stripped_html_path = fs.strip_root(outpath)
+    if not fs.exists(outpath):
+        raise ValueError("Rmdup could not generate the file " + stripped_html_path + " due to error " + err)
+
+    return stripped_html_path

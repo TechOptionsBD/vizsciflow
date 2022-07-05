faaeff002d546de2929660ed9dbff8174496491b
mainulhossain <mainulhossain@gmail.com>
2022-07-05 23:16:57 +0000

    #29:blasttoraw update from Chi

--- src/plugins/modules/blasttoraw/adapter.py
+++ src/plugins/modules/blasttoraw/adapter.py
@@ -6,8 +6,10 @@ thispath = path.dirname(__file__)
 def demo_service(context, *args, **kwargs):
 	blast_to_raw = path.join(thispath, 'quota-alignment', 'scripts', 'blast_to_raw.py')
 	
+	# parse user arguments
 	arguments = context.parse_args('BlastToRaw', 'blast', *args, **kwargs)
 	
+	# prepare output directories
 	outdir = context.createoutdir()
 	
 	blaststem = Path(arguments['blast']).stem
@@ -18,8 +20,18 @@ def demo_service(context, *args, **kwargs):
 	snodups = path.join(outdir, blaststem +'.s.nolocaldups.bed')
 	
 	out = path.join(outdir, "{0}.tdd{1}.cs{2}.filtered".format(blaststem, arguments['tandem_Nmax'], arguments['cscore']))
-	
-	cmdargs = [arguments['blast'], "--localdups --qbed=" + arguments['qbed'], "--sbed="+ arguments['sbed'], "--tandem_Nmax={0}".format(arguments['tandem_Nmax']), "--cscore={0}".format(arguments['cscore']), ">", out]
-	log, err = context.pyvenv_run(thispath, "python2", blast_to_raw, *cmdargs)
+
+
+	cmdargs = ["--localdups"] + ["=".join(["--"+key, str(arguments[key])]) for key in arguments.keys() if key != "blast"]
+	stdout, log = context.pyvenv_run(thispath, "python2", blast_to_raw, arguments["blast"], *cmdargs)
+
+
+	if log:
+		# display log report
+		context.out.append(str(log))
+
+	with open(out, "w") as file:
+		# write stdout to output file
+		file.write(stdout)
 	
 	return [qdups, qnodups, sdups, snodups, out]

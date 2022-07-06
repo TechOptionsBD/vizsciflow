9cb3b7d64f5280ce4f3651b8d30998a681f3235b
mainulhossain <mainulhossain@gmail.com>
2022-07-06 16:46:23 +0000

    #29: geneorder added from Chi

---
+++ src/plugins/modules/geneorder/adapter.py
@@ -0,0 +1,20 @@
+from os import path
+from pathlib import Path
+
+thispath = path.dirname(__file__)
+
+def gene_order(context, *args, **kwargs):
+    
+    geneorder = path.join(thispath, 'gene_order.py')
+    arguments = context.parse_args('GeneOrder', 'CoGe', *args, **kwargs)
+
+    output = path.join(context.createoutdir(), Path(arguments["input"]).stem + ".go")
+    input = arguments.pop("input")
+
+    cmdargs = [" ".join(["--"+pair[0], pair[1]]) for pair in arguments.items() if pair[1]]
+    stdout, err = context.pyvenv_run(thispath, "python2", geneorder, input, output, *cmdargs)
+    
+    if err:
+        raise ValueError("GeneOrder failed to generate " + output + " due to error: " + err)
+    
+    return output

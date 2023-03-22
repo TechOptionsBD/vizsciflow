829bcb2ee0874fe516bc1c897b297d0c320c7297
mainulhossain <mainulhossain@gmail.com>
2023-03-22 10:10:43 -0600

    #74: add new venv program demo

---
+++ src/plugins/modules/demos/newvenvprogram/adapter.py
@@ -0,0 +1,10 @@
+from os import path
+thispath = path.abspath(path.dirname(__file__))
+
+def demo_service(context, *args, **kwargs):
+	arguments = context.parse_args('SumInNewVenv', 'demo', *args, **kwargs)
+	# run in new virtual environment
+	out, err = context.pyvenv_run_at_venv(thispath, "python", 'newvenvprogram', path.join(thispath, 'sum.py'), arguments["data"])
+	if err:
+		raise(f"Tool SumInNewVenv exited with error: {err}")
+	return int(out) # exception is raised if sum.py doesn't write valid numeric values in stdout

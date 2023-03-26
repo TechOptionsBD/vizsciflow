65d2c137da2b6811963debf4d2d62f4759b47218
mainulhossain <mainulhossain@gmail.com>
2023-03-25 22:33:26 -0600

    #92: Change validator in RegistrationForm

--- src/app/auth/forms.py
+++ src/app/auth/forms.py
@@ -16,9 +16,9 @@ class RegistrationForm(Form):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64),
                                            Email()])
     username = StringField('Username', validators=[
-        DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+        DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_]*$', 0,
                                           'Usernames must have only letters, '
-                                          'numbers, dots or underscores')])
+                                          'numbers, or underscores')])
     password = PasswordField('Password', validators=[
         DataRequired(), EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])

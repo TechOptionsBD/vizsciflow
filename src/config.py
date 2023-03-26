1939f19c6da12b62717deb527c355dce4b368bd6
mainulhossain <mainulhossain@gmail.com>
2023-03-25 22:46:43 -0600

    #96: correct mail settings

--- src/config.py
+++ src/config.py
@@ -26,9 +26,9 @@ class Config:
     MAIL_USERNAME = ''
     MAIL_PASSWORD = ''
     BIOWL = os.path.join(ROOT_DIR, 'app/dsl/')
-    PHENOPROC_MAIL_SUBJECT_PREFIX = '[Phenoproc]'
-    PHENOPROC_MAIL_SENDER = 'Phenoproc Admin <phenoproc@gmail.com>'
-    PHENOPROC_ADMIN = os.environ.get('PHENOPROC_ADMIN')         
+    MAIL_SUBJECT_PREFIX = '[Phenoproc]'
+    MAIL_SENDER = 'Phenoproc Admin <phenoproc@gmail.com>'
+    ADMIN = os.environ.get('PHENOPROC_ADMIN')         
     PHENOPROC_POSTS_PER_PAGE = 20
     PHENOPROC_FOLLOWERS_PER_PAGE = 50
     PHENOPROC_COMMENTS_PER_PAGE = 30

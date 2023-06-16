a91ebd86e9ed3d0c62279f81379477757dff0a98
mainulhossain <mainulhossain@gmail.com>
2023-06-16 05:14:44 -0600

    #107: correct id parsing

--- src/plugins/modules/workflow/adapter.py
+++ src/plugins/modules/workflow/adapter.py
@@ -4,5 +4,7 @@ from app.main.jobsview import run_biowl_internal
 def run_workflow(context, *args, **kwargs):
     id = kwargs.pop('id', None)
     if not id:
-        id = args.pop()
+        if not args:
+            raise ValueError("No workflow id is given.")
+        id = int(args[0])
     return run_biowl_internal(id, context.user_id, None, kwargs)

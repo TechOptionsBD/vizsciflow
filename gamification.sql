77d29a448e9eed247dd39972231a022c2ff02aa7
CalebCalnek <azo062@mail.usask.ca>
2026-04-06 12:32:37 -0600

    create sql file to create tables for gamification data

---
+++ gamification.sql
@@ -0,0 +1,33 @@
+--
+-- gamification data storage
+--
+
+\restrict vp5AOohTfP0QlBHRKlwLxgU0ZvJIIGcfIRifX0CRPw6P5tWG5nRdSyv6LBOCUyC
+
+--
+-- Name: game_points; Type: TABLE; Schema: public; Owner: phenodoop
+--
+
+CREATE TABLE public.game_points (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    title text,
+    record_time timestamp without time zone
+);
+
+ALTER TABLE public.game_points OWNER TO phenodoop;
+
+--
+-- Name: game_usage_history; Type: TABLE; Schema: public; Owner: phenodoop
+--
+
+CREATE TABLE public.game_usage_history (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    record_time date
+);
+
+ALTER TABLE public.game_points OWNER TO phenodoop;
+
+\unrestrict vp5AOohTfP0QlBHRKlwLxgU0ZvJIIGcfIRifX0CRPw6P5tWG5nRdSyv6LBOCUyC
+

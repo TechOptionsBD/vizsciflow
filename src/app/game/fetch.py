0012652f5694d1c6fec0d723248dfcb969bab6e5
CalebCalnek <azo062@mail.usask.ca>
2026-05-19 12:09:44 -0600

    change gamification internal user id retrieval to use existing user manager

--- src/app/game/fetch.py
+++ src/app/game/fetch.py
@@ -1,14 +1,10 @@
 from .. import db
 from .managers import *
+from ..managers.usermgr import usermanager
 from datetime import datetime
 
 def get_user_id(username):
-    command = "SELECT * FROM public.users WHERE username = %(username)s"
-    result = db.engine.execute(command, {"username": username})
-    rows = result.mappings()
-    row = rows.first()
-    user_id = row["id"]
-    return user_id
+    return usermanager.get_by_username(username).id
 
 def points_entry_latest(username):
     user_id = get_user_id(username)
